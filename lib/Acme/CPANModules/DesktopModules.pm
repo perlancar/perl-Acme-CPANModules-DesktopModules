475213ef23d7743ba4348bbd0a5b09e8333b7df8
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-10-17 20:14:26 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/DesktopModules.pm
@@ -0,0 +1,18 @@
+package Acme::CPANModules::DesktopModules;
+
+# DATE
+# VERSION
+
+our $LIST = {
+    summary => "Modules related to GUI desktop environment",
+    entries => [
+        {module=>'Desktop::Detect'},
+        {module=>'Screensaver::Any'},
+        {module=>'App::ScreensaverUtils'},
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
